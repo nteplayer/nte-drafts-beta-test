f2c864da59a70714f4e6e5d767f2aac50d278e59
nteplayer <rusin.dima1103@gmail.com>
2026-05-26 19:26:43 +0500

    Update app.js

--- project/app.js
+++ project/app.js
@@ -1,1 +1,87 @@
 
+import { auth }
+
+from "./firebase.js";
+
+import {
+
+    createUserWithEmailAndPassword,
+
+    signInWithEmailAndPassword
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    if(password.length < 6){
+
+        alert("Password must contain at least 6 characters");
+
+        return;
+    }
+
+    try{
+
+        await createUserWithEmailAndPassword(
+
+            auth,
+
+            email,
+
+            password
+
+        );
+
+        alert("Account created successfully");
+
+    }
+
+    catch(error){
+
+        alert(error.message);
+
+    }
+
+});
+
+loginBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    try{
+
+        await signInWithEmailAndPassword(
+
+            auth,
+
+            email,
+
+            password
+
+        );
+
+        alert("Login successful");
+
+        window.location.href = "pages/profile.html";
+
+    }
+
+    catch(error){
+
+        alert(error.message);
+
+    }
+
+});
