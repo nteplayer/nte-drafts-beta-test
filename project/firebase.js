79c3c0aaa7d1d2006ac91a8455ab5f0511cf9a0a
nteplayer <rusin.dima1103@gmail.com>
2026-05-26 19:26:30 +0500

    Update firebase.js

--- project/firebase.js
+++ project/firebase.js
@@ -1,1 +1,42 @@
+import { initializeApp }
 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+
+    getAuth
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+
+    getFirestore
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAa_zvBCLyFZKcclMtUfRs_2Tx__JXcTP4",
+
+    authDomain: "nte-tournaments-7b0e3.firebaseapp.com",
+
+    databaseURL: "https://nte-tournaments-7b0e3-default-rtdb.firebaseio.com/",
+
+    projectId: "nte-tournaments-7b0e3",
+
+    storageBucket: "nte-tournaments-7b0e3.appspot.com",
+
+    messagingSenderId: "1022034023092",
+
+    appId: "1:1022034023092:web:d269068348b24abed93de1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
