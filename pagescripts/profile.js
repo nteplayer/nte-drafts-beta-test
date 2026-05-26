e11064ced77c8ec2aed788202933db0e18491822
nteplayer <rusin.dima1103@gmail.com>
2026-05-26 19:46:05 +0500

    Create profile.js

---
+++ pagescripts/profile.js
@@ -0,0 +1,33 @@
+import { auth }
+
+from "../firebase.js";
+
+import {
+
+    onAuthStateChanged,
+
+    signOut
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if(user){
+
+        document.getElementById("emailText").innerText =
+            user.email;
+
+        document.getElementById("nickname").innerText =
+            user.email.split("@")[0];
+
+    }
+
+    else{
+
+        window.location.href = "../index.html";
+
+    }
+
+});
